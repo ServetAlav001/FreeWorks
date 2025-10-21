9759a925eac933fd36f16454e8d603818c5fe33d
servet alav <servetalav5@gmail.com>
2025-10-21 17:27:24 +0300

    Initialize Java Swing application with look and feel
    Set the system look and feel for the Swing application and launch the main window.

---
+++ Java Swing Library/first example/main.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                AnaEkran anaEkran = new AnaEkran();
+                anaEkran.setVisible(true);
+            }
+        });
+
+
+    }
+}
