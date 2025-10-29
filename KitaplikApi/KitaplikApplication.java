6e509836b0349929f5ab726f92d332c638ace2ef
servet alav <servetalav5@gmail.com>
2025-10-30 02:20:47 +0300

    Create KitaplikApplication.java for Spring Boot app

---
+++ KitaplikApi/KitaplikApplication.java
@@ -0,0 +1,13 @@
+package com.deneme.KitaplikApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitaplikApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitaplikApiApplication.class, args);
+	}
+
+}
