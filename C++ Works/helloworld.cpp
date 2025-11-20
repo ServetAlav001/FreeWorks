da162f7cc8da4843aa0490999dc3baf38a7a75ee
servet alav <servetalav5@gmail.com>
2025-11-20 13:20:49 +0300

    Add hello world program with sum calculation

---
+++ C++ Works/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main() {
+
+    std::cout << "hello world\n";
+    int sayi1;
+    int sayi2;
+    int toplam;
+    std::cout << "bir sayi giriniz:";
+    std::cin>>sayi1;
+    std::cout<<"bir sayi daha giriniz:";
+    std::cin>>sayi2;
+    toplam=sayi1+sayi2;
+    std::cout<<"sayilarin toplami: "<<toplam;
+    
+}
