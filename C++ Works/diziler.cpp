f51c1639d3434ecb80ae3ae239f02ad4fe54d1d9
servet alav <servetalav5@gmail.com>
2025-11-24 21:19:45 +0300

    Add files via upload

---
+++ C++ Works/diziler.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    /*double toplam = 0;
+    double ortalama;
+    double dersler[5];
+    for (int i = 0; i < 5; i++) {
+        std::cout << (i + 1) << ".inci ogrenci notu:" << std::endl;
+        std::cin >> dersler[i];
+        toplam += dersler[i];
+    }
+    ortalama = toplam / 5;
+    std::cout << "ogrencilerin not ortalamasi: "<<ortalama;*/
+
+    int a = 100;
+    int b = 200;
+    int* p1 = &a;
+    int* p2 = &b;
+    int toplam;
+    toplam = *p1 + *p2;
+    std::cout << "toplam: " << toplam;
+    *p1 = 500;
+    std::cout << "\na nin yeni degeri: " << a;
+    
+}
+
+
+
