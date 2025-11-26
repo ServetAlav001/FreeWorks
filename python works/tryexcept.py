6c787d1dcdee70d3a1774c3f7487fccddfd891a7
servet alav <servetalav5@gmail.com>
2025-11-26 19:48:27 +0300

    Update tryexcept.py

--- python works/tryexcept.py
+++ python works/tryexcept.py
@@ -2,7 +2,13 @@ try:
     x=int(input("x: "))
     y=int(input("y: "))
     print(x/y)
-except ZeroDivisionError:
-    print("bir sayi sifira bolunemez...")
-except ValueError:
-    print("bir sayi giriniz bir harf degil")
+#except ZeroDivisionError:
+    #print("bir sayi sifira bolunemez...")
+#except ValueError:
+   #print("bir sayi giriniz bir harf degil")
+except Exception as ex:
+    print("bir hata olustu..." , ex)
+else :
+    print("hersey yolunda")
+finally:
+    print("blok kapandı")
