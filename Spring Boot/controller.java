b7cd9aab0b3ea756a54af5707d2c250a8b7114a8
servet alav <servetalav5@gmail.com>
2025-12-02 22:08:33 +0300

    Add HelloController with /hello endpoint

---
+++ Spring Boot/controller.java
@@ -0,0 +1,13 @@
+package com.servetalav.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+
+    //localhost:8080/hello
+    @GetMapping(path = "/hello")
+    public String sayhello() {
+        return "hello world";
+    }
+}
