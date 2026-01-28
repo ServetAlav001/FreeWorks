ef0905a2b9561f21d5119369292916fa2a842845
servet alav <servetalav5@gmail.com>
2026-01-28 23:40:48 +0300

    Create App component with Counter integration

---
+++ React Works/components and state/app.jsx
@@ -0,0 +1,10 @@
+import Counter from "./components/Counter";
+import Header  from "./components/Header";
+ function App() {
+  return (
+  <div>
+    <Counter/>
+  </div>
+  )
+}
+export default App;
